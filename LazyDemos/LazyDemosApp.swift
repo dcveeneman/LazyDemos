82e4b57cc37f039d3cd012ac0b64b1dc166e7529
David Veeneman <davidv@veeneman.com>
2023-11-24 10:03:33 -0800

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by David Veeneman on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
