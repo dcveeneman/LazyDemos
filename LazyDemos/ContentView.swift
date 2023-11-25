5f48e0b171a690ef1cdd14f139d4a099c8c5ec81
David Veeneman <davidv@veeneman.com>
2023-11-24 16:10:44 -0800

    2023/11/24 - Gallaugher Chapter 4.4: LazyVGrid with adaptive columns.

--- LazyDemos/ContentView.swift
+++ LazyDemos/ContentView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100), spacing: 5)], spacing: 5) {
+                ForEach(0..<1000) {i in
+                    RedAndCyanView()
+                }
+            }
         }
         .padding()
+    }
+}
+
+struct RedAndCyanView: View {
+    var body: some View {
+        ZStack {
+            Color(.red)
+            Rectangle()
+                .fill(.cyan)
+                .frame(width: 100, height: 100)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
